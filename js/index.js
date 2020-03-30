690ef370d0e8a70a7aafefb63a21f08fbd283a85
Harry Garrood <harry@garrood.me>
2020-03-30 23:03:54 +0100

    wip

--- js/index.js
+++ js/index.js
@@ -4,8 +4,10 @@ let defaults = {
   wavelength: 20,
   // transmitter separation (as a proportion of wavelength)
   separation: 0.5,
-  // number of transmitters
-  transmitters: 5,
+  // number of transmitter groups
+  transmitterGroups: 6,
+  // number of transmitters per group
+  transmittersPerGroup: 10,
   // angle to aim transmitters (using phase differences)
   transmitAngle: 0,
   // receiver distance
@@ -18,6 +20,10 @@ function deg2rad(deg) {
   return deg / 180 * Math.PI;
 }
 
+function rad2deg(rad) {
+  return rad * 180 / Math.PI;
+}
+
 function distance(a, b) {
   let xdiff = a.x - b.x;
   let ydiff = a.y - b.y;
@@ -28,28 +34,12 @@ function count_wavelengths(params, a, b) {
   return distance(a, b) / params.wavelength;
 }
 
-// Get the phase shift for the nth transmitter (starting from the left, i.e.
-// largest negative x, counting from 0). Returns turns (i.e. any integer means
-// 'in phase').
-function get_phase_shift(params, index) {
-  // imagine a right-angled triangle where the hypotenuse is the line
-  // connecting two adjacent transmitters
-  let hyp = params.separation;
-  // theta is the angle between the transmit beam and the x-axis (along which
-  // the transmitters lie).
-  let theta = deg2rad(90 - params.transmitAngle);
-  // This is the difference that the signals from two adjacent transmitters
-  // have to travel to reach a certain point, in wavelengths
-  let delta = params.separation * Math.cos(theta);
-  return index * delta;
-}
-
 function get_wave_from_transmitter(params, t, r) {
   return {
     scale: 1,
     phaseShift: 2 * Math.PI *
       ( count_wavelengths(params, t, r) + // phase shift from distance
-        get_phase_shift(params, t.index) // phase shift from array
+        params.groupPhaseShiftFunc(params, t.group) // phase shift from transmitter group
       )
   };
 }
@@ -70,11 +60,14 @@ function add_waves(x, y) {
 
 function get_model_from_params(params) {
   let separationMM = params.wavelength * params.separation;
-  let arrayWidth = (params.transmitters - 1) * separationMM;
-  let transmitters = Array(params.transmitters).fill(null).map((_, i) => {
+  let numTransmitters = params.transmittersPerGroup * params.transmitterGroups;
+  let arrayWidth = (numTransmitters - 1) * separationMM;
+  let transmitters = Array(numTransmitters).fill(null).map((_, i) => {
+    let group = Math.floor(i / params.transmittersPerGroup);
     return {
-      index: i,
-      x: -arrayWidth/2 + (i * separationMM),
+      group,
+      index: i % params.transmittersPerGroup,
+      x: i * separationMM,
       y: 0
     };
   });
@@ -90,10 +83,12 @@ function get_model_from_params(params) {
 function params_from_dom() {
   return {
     wavelength: Number(document.getElementById('wavelength').value),
-    separation: Number(document.getElementById('separation').value),
-    transmitters: Number(document.getElementById('transmitters').value),
-    transmitAngle: Number(document.getElementById('transmitAngle').value),
     distance: Number(document.getElementById('distance').value) * 1000,
+    separation: Number(document.getElementById('separation').value),
+    transmittersPerGroup: Number(document.getElementById('transmittersPerGroup').value),
+    transmitterGroups: Number(document.getElementById('transmitterGroups').value),
+    transmitAngle: Number(document.getElementById('transmitAngle').value),
+    groupPhaseShiftFunc: new Function('params', 'group', document.getElementById('groupPhaseShiftFunc').value)
   };
 }
 
@@ -133,28 +128,29 @@ function drawChart() {
 }
 
 function drawCanvas() {
-  let canvas = document.getElementById('canvas');
+  // Draw one group on the canvas
+  let canvas = document.getElementById('transmitter-group-canvas');
   let ctx = canvas.getContext('2d');
   let canvas_width = 400;
   let canvas_height = 200;
   ctx.clearRect(0, 0, canvas_width, canvas_height);
 
   let params = params_from_dom();
-  // width of the whole array in wavelengths
-  let arrayWidth = (params.transmitters - 1) * params.separation;
-  // want the whole canvas to be a bit bigger than the width of the array
-  let pixels_per_wavelength = canvas_width / (arrayWidth * 1.2);
+  // width of a group in wavelengths
+  let groupWidth = (params.transmittersPerGroup - 1) * params.separation;
+  // want the whole canvas to be a bit bigger than the width of the group
+  let pixels_per_wavelength = canvas_width / (groupWidth * 1.2);
   let pixels_per_mm = pixels_per_wavelength / params.wavelength;
 
   let model_to_canvas = (({ x, y }) => {
     return {
-      x: (canvas_width / 2) + (x * pixels_per_mm),
+      x: (canvas_width / 10) + (x * pixels_per_mm),
       y: (canvas_height * 0.8) - (y * pixels_per_mm)
     };
   });
 
   let model = get_model_from_params(params);
-  model.transmitters.forEach((t, i) => {
+  model.transmitters.filter(t => t.group == 0).forEach((t, i) => {
     let transmitter = model_to_canvas(t);
 
     let line_length = canvas_width;
@@ -187,14 +183,13 @@ function updateDisplayTexts() {
       el.innerText = String(params[k]);
     }
   });
-  let shift = get_phase_shift(params, 1) * 360;
-  document.getElementById('phase-shift').innerText =
-    "Phase shift between adjacent transmitters is " + shift.toFixed(2) + " degrees.";
 }
 
 draw();
 
-Array.from(document.getElementsByTagName('input')).forEach(i => {
-  i.addEventListener('change', draw);
-  i.addEventListener('input', draw);
+['input', 'textarea'].forEach(tagName => {
+  Array.from(document.getElementsByTagName(tagName)).forEach(i => {
+    i.addEventListener('change', draw);
+    i.addEventListener('input', draw);
+  });
 });
